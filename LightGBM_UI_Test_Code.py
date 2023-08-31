67a658e715d65533c928b9ba0fce714040123469
Dhingraparag368 <35643312+Dhingraparag368@users.noreply.github.com>
2023-09-01 00:24:36 +0530

    Add files via upload

--- LightGBM_UI_Test_Code.py
+++ LightGBM_UI_Test_Code.py
@@ -19,185 +19,83 @@ import lightgbm as lgb
 from Support_file import *
 
 
-st.header('Light GBM Regression UI')
-
-
-# Input LGBM Parameters
-with st.expander('Input LGBM Parameters'):
-    n_trees = st.slider('Number of Trees', min_value=1, max_value=1000, value= 200)
-    tree_depth = st.slider('Depth of Trees', min_value=1, max_value=10, value= 5)
-    min_leaf_size = st.slider('Number of leaves in Trees', min_value=4, max_value=100, value= 15)
-    learning_rate = st.slider('Learning Rate ', min_value=1, max_value=1000, value= 10)
-    subsample = st.slider('Row Sample ', min_value=0.5, max_value=1.0, value= 0.8)
-    colsample_bytree = st.slider('Column Sample ', min_value=0.5, max_value=1.0, value= 0.8)
-    bagging_freq = st.slider('Bagging Freq ', min_value=1, max_value=10, value= 2)
-    
-    
-    tree_params = {'n_trees':n_trees, 'tree_depth':tree_depth, 'min_leaf_size': min_leaf_size, \
-                   'learning_rate': learning_rate/1000.0, 'subsample': subsample, 'colsample_bytree': colsample_bytree,\
-                   'bagging_freq': bagging_freq}
-# Input LGBM Parameters Ends
-
-
-# Widget for uploading file
-uploaded_file = st.file_uploader(label = 'Upload file for regression', type=("csv"))
-
-# Columns/Features have to come from CSV
-# Currently based on static list
 
 feat_contri_dict = {}
 feat_mono_dict = {}
 exclude_from_model = {}
 categorical_cols = {}
-    
+key_counter = 1
+granularity_list = []
+granularity_filter_selected_dict = {}
+# To be provided as Input by User
+use_date_for_group_by = True
+groupby_var = 'DATE'
+potential_target_var_names = ['TARGET', 'SALES', 'UNITS']
+target_var = ''
+
+
+
+st.header('Light GBM Regression UI')
+
+
+# Input LGBM Parameters
+tree_params = lightgbm_hyperparam_block()
+
+
+# Widget for uploading file
+uploaded_file = st.file_uploader(label = 'Upload file for regression', type=("csv"))
+
 
 if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
-    dataframe['DATE'] = pd.to_datetime(dataframe['DATE'])
-    # st.write(dataframe)
-    
-    # break
-    
+    dataframe.columns = [x.upper() for x in dataframe.columns]
     column_list = dataframe.columns
     
-    target_var = st.selectbox("Select Name of Target/Dependent Variable", column_list)
+    if 'DATE' in dataframe.columns:
+        dataframe['DATE'] = pd.to_datetime(dataframe['DATE'])
+
+    
+    # Setting Target Variable
+    target_var = set_target_var_block(potential_target_var_names, column_list)
+    
+    # Setting feature list
     feature_list = [x for x in column_list if x != target_var]
     
-    with st.expander("Select Categorical Columns: "):
-        
-        for col in feature_list:
-            categorical_cols[col] = st.checkbox('Is '+col+' categorical', value=False)
+    # Category col block
+    categorical_cols = set_categorical_cols_block(feature_list, categorical_cols)
     
     
-    with st.sidebar:
-        
-        for col in feature_list:
-            with st.expander(col):
-                feat_contri_dict[col] = st.slider("Contribution "+ col, min_value=0, max_value=100, value=100)
-                        
-                feat_mono_dict[col] = st.selectbox(
-                    'How '+col+' and Target variable are correlated?',
-                    ('Neutral', 'Positive', 'Negative'))
-                
-                exclude_from_model[col] = st.checkbox(
-                    'Exclude '+col+' from Model', value=False)
-            
+    # Code for Side Bar
+    feature_list, feat_contri_dict, feat_mono_dict, exclude_from_model = side_bar_for_features_contri_mono(feature_list, feat_contri_dict, feat_mono_dict, exclude_from_model)
     
-    for col in exclude_from_model:
-        if exclude_from_model[col]:
-            feature_list = [x for x in feature_list if x != col]
-            feat_contri_dict.pop(col)
-            feat_mono_dict.pop(col)
-            
-    granularity_list = ['BRAND', 'CENSUS_TRADING_AREAS', 'PACK SIZE']
-    
-    
-    # st.write(factor_df)
-    
-    # Scaling DF, for better results
-    factor_df = scale_data(dataframe, granularity_list, target_var)
-    data_rows = dataframe.shape[0]
-    dataframe = dataframe.merge(factor_df, on= granularity_list, how = 'left')
-    assert dataframe.shape[0] == data_rows
-    dataframe[target_var + '_new'] = dataframe[target_var]/dataframe['factor']
 
-    target_var_old = target_var
-    target_var = target_var + '_new'
+    # Granularity, May not be used in later code, more like a replacement for categorical dict
+    granularity_list = [x for x in categorical_cols if categorical_cols[x]]
+    dataframe['granularity_col'] = ''
+    for x in granularity_list:
+        dataframe['granularity_col'] = dataframe['granularity_col'] + ' ' + dataframe[x].astype('str')
+        
+    # Scale Data for better Accuracy
+    dataframe, target_var_old, target_var = scale_data_block(dataframe, granularity_list, target_var)
     
     #Running Model
-    data, model = model_func(dataframe, target_var, target_var_old, feature_list, list(feat_contri_dict.values()), list(feat_mono_dict.values()), \
-               categorical_cols, exclude_from_model, tree_params)
-        
+    data, model = model_func(dataframe, target_var, target_var_old, feature_list, \
+                             feat_contri_dict, feat_mono_dict, \
+                             categorical_cols, exclude_from_model, tree_params)
+    
+    
     with st.expander('Feature Importance: '):
-        # Plotting Feature Importance
-        # Plot is inside function, returns None
         get_feature_importance_chart(model)
-        
-    with st.expander('Actual Vs Predicted: '):
-        st.write('')
-        st.write('Filter for AVP Plot')
-        
-        brand_selected = st.selectbox(
-            'Select Brand: ',
-            list(data['BRAND'].unique()) + ['None'], key = 1)
-        
-        pack_size_selected = st.selectbox(
-            'Select PACK SIZE: ',
-            list(data['PACK SIZE'].unique())+ ['None'], key = 2)
-        
-        CTA_selected = st.selectbox(
-            'Select CTA: ',
-            list(data['CENSUS_TRADING_AREAS'].unique()) + ['None'], key = 3)
-        
-        temp = data.copy()
-        if brand_selected != 'None':
-            temp = temp[temp['BRAND'] == brand_selected]
-            
-        if pack_size_selected != 'None':
-            temp = temp[temp['PACK SIZE'] == pack_size_selected]
-            
-        if CTA_selected != 'None':
-            temp = temp[temp['CENSUS_TRADING_AREAS'] == CTA_selected]
-            
-        
-        chart_data = temp.groupby('DATE')[[target_var_old, 'predicted_old']].sum().reset_index()
-        chart_data = chart_data.sort_values(by = 'DATE', ascending = False)
-        st.line_chart(chart_data, x ='DATE', y = [target_var_old, 'predicted_old'])
     
+    # Actual Vs Predicted Widget
+    key_counter = actual_vs_predicted_block(data, granularity_list, key_counter, column_list, target_var_old)
     
+
     # Base Vs Incremental (Total)
-    with st.expander('Aggregated Base Vs Incremental: '):
-        agg_base_inc_df = get_base_contribution(data, model, target_var_old, feature_list, feat_mono_dict, granularity_list)
+    data, key_counter = base_vs_incremental_block(data, model, target_var_old, feature_list, feat_mono_dict, column_list, key_counter)
         
-        brand_selected = st.selectbox(
-            'Select Brand: ',
-            list(data['BRAND'].unique()) + ['None'], key = 4)
-        
-        pack_size_selected = st.selectbox(
-            'Select PACK SIZE: ',
-            list(data['PACK SIZE'].unique()) + ['None'], key = 5)
-        
-        CTA_selected = st.selectbox(
-            'Select CTA: ',
-            list(data['CENSUS_TRADING_AREAS'].unique())+ ['None'], key = 6)
-        
-        temp = agg_base_inc_df.copy()
-        if brand_selected != 'None':
-            temp = temp[temp['BRAND'] == brand_selected]
-            
-        if pack_size_selected != 'None':
-            temp = temp[temp['PACK SIZE'] == pack_size_selected]
-            
-        if CTA_selected != 'None':
-            temp = temp[temp['CENSUS_TRADING_AREAS'] == CTA_selected]
-        
-        chart_data = temp.groupby('DATE')[[target_var_old, 'base_predict', 'predicted_old', 'incremental']].sum().reset_index()
-        chart_data = chart_data.sort_values(by = 'DATE', ascending = False)
-        st.line_chart(chart_data, x = 'DATE', y = [target_var_old, 'predicted_old', 'base_predict', 'incremental'])
-        
-    # Base Vs Incremental Ends
-    
-    
-    # Feature wise Incremental/Simulation
-    with st.expander('Feature wise Incremental/Simulation '):
-        st.write('')
-        st.write('Select Variable to see relationship with Target:')
-        
-        simulation_variables = []
-        for var in feature_list:
-            if var not in granularity_list:
-                for excluded in exclude_from_model:
-                    if exclude_from_model[excluded]:
-                        if var != excluded:
-                            simulation_variables.append(var)
-                            
-        if target_var in simulation_variables:
-            simulation_variables.remove(target_var)
-        
-        variable_selected = st.selectbox(
-            'Select Variable: ', simulation_variables)
-        
-        simulation_data = get_contribution(data, model, feature_list, variable_selected, granularity_list)
-        
-#        sim_chart_data = simulation_data.sum()
-#        st.line_chart(sim_chart_data)
+    # Feature Wise Contribution
+    data, key_counter = variable_contribution_block(data, model, feature_list, exclude_from_model, feat_mono_dict, 
+                                    granularity_list, target_var, target_var_old, key_counter)
+
+    download_model_data_block(data)
--- Support_file.py
+++ Support_file.py
@@ -14,9 +14,8 @@ import numpy as np
 
 
 
-
-def model_func(data, target_var, target_var_old, feature_list, feat_contri_list, \
-               monotone_list, categorical_cols, exclude_from_model, tree_params):
+def model_func(data, target_var, target_var_old, feature_list, feat_contri_dict, \
+               feat_monot_dict, categorical_cols, exclude_from_model, tree_params):
     
     #Data Prep
     for col in categorical_cols:
@@ -26,15 +25,23 @@ def model_func(data, target_var, target_var_old, feature_list, feat_contri_list,
     
     # st.write(feature_list)
     monotone_list_int = []
-    for i in monotone_list:
-        if i == 'Positive':
+    for var in feature_list:
+        if feat_monot_dict[var] == 'Positive':
             monotone_list_int.append(1)
-        elif i == 'Negative':
+        elif feat_monot_dict[var] == 'Negative':
             monotone_list_int.append(-1)
-        else:
+        elif feat_monot_dict[var] == 'Neutral':
             monotone_list_int.append(0)
             
     
+    contri_list= []
+    for var in feature_list:
+        if var in feat_contri_dict:
+            contri_list.append(feat_contri_dict[var]/100)
+            
+    # st.write(len(monotone_list_int), len(feature_list), len(contri_list))
+    # st.write(monotone_list_int)
+    # st.write(feature_list)
     
     tree = tree_params['n_trees']
     depth = tree_params['tree_depth']
@@ -54,31 +61,22 @@ def model_func(data, target_var, target_var_old, feature_list, feat_contri_list,
         st.write(data)
     
     model = lgb.LGBMRegressor(num_tree = tree, max_depth = depth, min_data_in_leaf = leaf_size, \
-                              feature_contri = feat_contri_list, monotone_constraints = monotone_list_int,\
+                              feature_contri = contri_list, monotone_constraints = monotone_list_int,\
                               colsample_bytree = colsample_bytree, learning_rate = learning_rate, \
                               subsample = subsample, bagging_freq = bagging_freq)
 
-    # st.write('')
-    # st.write(str(model))
-    # st.write(target_var)
     
     X_train = data[feature_list]
     Y_train = data[target_var]
     
-    # st.write(X_train.dtypes)
-    # st.write(Y_train.dtypes)
-    
-    # st.write(str(type(X_train)))
-    # st.write(str(type(Y_train)))
-    
-    # st.write(X_train)
     
     model.fit(X_train, Y_train)
     
-    # st.write('After Model Fit')
+    
     data['predicted'] = model.predict(X_train)
     data['predicted'] = np.where(data['predicted']< 0, 0, data['predicted'])
     data['predicted_old'] = data['predicted']*data['factor']
+    data['abs_error'] = np.abs(data[target_var_old] - data['predicted_old'])
     
     with st.expander("Model Accuracy Metrics: "):
         st.write('')
@@ -124,8 +122,7 @@ def scale_data(data, scale_groupby, scale_col):
 def get_contribution(data, model, feature_list, variable, granularity_list):
     
     output_cols = []
-    data['base_predict'] = model.predict(data[feature_list])
-    output_cols.append('base_predict')
+    output_cols.append('predicted_old')
     
     st.write('Starting simulation process...')
     
@@ -145,20 +142,276 @@ def get_contribution(data, model, feature_list, variable, granularity_list):
 
 
 
-def get_base_contribution(data, model, target_var_old, feature_list, feat_mono_dict, granularity_list):
+def get_base_contribution(data, model, feature_list, feat_mono_dict, feature = ''):
     
     temp = data.copy()
-    for feat in feat_mono_dict:
-        if feat_mono_dict[feat] == 'Positive':
-            temp[feat] = temp[feat].min()
-        
-        if feat_mono_dict[feat] == 'Negitive':
-            temp[feat] = temp[feat].max()
+    
+    if feature == '':
+        for feat in feature_list:
+            if feat_mono_dict[feat] == 'Positive':
+                temp[feat] = temp[feat].min()
             
-    data['base_predict'] = model.predict(temp[feature_list])
-    data['base_predict'] = data['base_predict']*data['factor']
-    data['incremental'] = data['predicted_old'] - data['base_predict']
+            if feat_mono_dict[feat] == 'Negitive':
+                temp[feat] = temp[feat].max()
+                
+        data['base_predict'] = model.predict(temp[feature_list])
+        data['base_predict'] = data['base_predict']*data['factor']
+        data['incremental']  = data['predicted_old'] - data['base_predict']
+        
+    else:
+        if feat_mono_dict[feature] == 'Positive':
+            temp[feature] = temp[feature].min()
+        
+        if feat_mono_dict[feature] == 'Negitive':
+            temp[feature] = temp[feature].max()
+            
+        data['predict_'+ feature] = model.predict(temp[feature_list])
+        data['predict_'+ feature] = data['predict_'+ feature]*data['factor']
+        data['inc_'+ feature]  = data['predicted_old'] - data['predict_'+ feature]
+
+    
+    return data
+
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
 
 
-    return data.groupby(granularity_list + ['DATE'])[[target_var_old, 'predicted_old', 'base_predict', 'incremental']]\
-        .sum().reset_index()
+
+def set_target_var_block(potential_target_var_names, column_list):
+    
+    target_var = ''
+    
+    for var in potential_target_var_names:
+        if var in column_list:
+            target_var = var
+        
+    if target_var == '':
+        target_var = st.selectbox("Select Name of Target/Dependent Variable", column_list)
+        
+    return target_var
+
+
+def set_categorical_cols_block(feature_list, categorical_cols):
+    
+    with st.expander("Select Categorical Columns: "):
+        
+        for col in feature_list:
+            categorical_cols[col] = st.checkbox('Is '+col+' categorical', value=False)
+            
+    return categorical_cols
+
+
+def side_bar_for_features_contri_mono(feature_list, feat_contri_dict, feat_mono_dict, exclude_from_model):
+    
+    with st.sidebar:
+        
+        for col in feature_list:
+            with st.expander(col):
+                feat_contri_dict[col] = st.slider("Contribution "+ col, min_value=0, max_value=200, value=100)
+                        
+                feat_mono_dict[col] = st.selectbox(
+                    'How '+col+' and Target variable are correlated?',
+                    ('Neutral', 'Positive', 'Negative'))
+                
+                exclude_from_model[col] = st.checkbox(
+                    'Exclude '+col+' from Model', value=False)
+                
+            
+    
+    for col in exclude_from_model:
+        if exclude_from_model[col]:
+            feature_list = [x for x in feature_list if x != col]
+            feat_contri_dict.pop(col)
+            feat_mono_dict.pop(col)
+                
+    return feature_list, feat_contri_dict, feat_mono_dict, exclude_from_model
+
+
+def scale_data_block(dataframe, granularity_list, target_var):
+    
+    with st.expander('Scale Data: '):
+        do_scale = st.checkbox('Tick if data needs to be Scaled at Categorical Columns: ', value = False)
+    
+    # Scaling DF, for better results
+    # st.write(dataframe.columns)
+    if (len(granularity_list) > 0) & (do_scale):
+        factor_df = scale_data(dataframe, granularity_list, target_var)
+        data_rows = dataframe.shape[0]
+        dataframe = dataframe.merge(factor_df, on= granularity_list, how = 'left')
+        assert dataframe.shape[0] == data_rows
+        dataframe[target_var + '_new'] = dataframe[target_var]/dataframe['factor']
+
+
+    else:
+        dataframe['factor'] = 1
+        dataframe[target_var + '_new'] = dataframe[target_var]
+
+        
+    target_var_old = target_var
+    target_var = target_var + '_new'
+    
+    return dataframe, target_var_old, target_var
+
+def lightgbm_hyperparam_block():
+    
+    with st.expander('Input LGBM Parameters'):
+        n_trees = st.slider('Number of Trees', min_value=1, max_value=1000, value= 200)
+        tree_depth = st.slider('Depth of Trees', min_value=1, max_value=10, value= 5)
+        min_leaf_size = st.slider('Number of leaves in Trees', min_value=4, max_value=100, value= 15)
+        learning_rate = st.slider('Learning Rate ', min_value=1, max_value=1000, value= 10)
+        subsample = st.slider('Row Sample ', min_value=0.5, max_value=1.0, value= 0.8)
+        colsample_bytree = st.slider('Column Sample ', min_value=0.5, max_value=1.0, value= 0.8)
+        bagging_freq = st.slider('Bagging Freq ', min_value=1, max_value=10, value= 2)
+        
+        
+        tree_params = {'n_trees':n_trees, 'tree_depth':tree_depth, 'min_leaf_size': min_leaf_size, \
+                       'learning_rate': learning_rate/1000.0, 'subsample': subsample, 'colsample_bytree': colsample_bytree,\
+                       'bagging_freq': bagging_freq}
+            
+    return tree_params
+
+def base_vs_incremental_block(data, model, target_var_old, feature_list, feat_mono_dict, column_list, key_counter):
+    
+    with st.expander('Aggregated Base Vs Incremental: '):
+        
+        data = get_base_contribution(data, model, feature_list, feat_mono_dict)
+        
+        temp = data.copy()
+        
+        filter_cols = st.multiselect('Select Columns for filter', column_list, key = key_counter)
+        key_counter = key_counter + 1
+        
+
+        for filter_col in filter_cols:
+            type_of_col = str(data[filter_col].dtype)
+            
+            if ('int' in type_of_col) or ('float' in type_of_col):
+                
+                start_val, end_val = st.select_slider('Select Range for '+filter_col, \
+                                                      range(0, int(data[filter_col].max()+1)), \
+                                                      value = (0, int(data[filter_col].max()-1)), \
+                                                      key = key_counter)
+                key_counter = key_counter + 1
+                    
+                temp = temp[(temp[filter_col] >= start_val) & (temp[filter_col] <= end_val)]
+            
+            else:
+                
+                filter_values = st.multiselect('Select Values for '+ filter_col, data[filter_col].unique(), \
+                                               key = key_counter)
+                key_counter = key_counter + 1
+                temp = temp[temp[filter_col].isin(filter_values)]
+        
+
+        x_axis = st.selectbox('Select Column for X Axis ', column_list, key = key_counter)
+        key_counter = key_counter + 1
+
+        chart_data = temp.groupby(x_axis)[[target_var_old, 'base_predict', 'predicted_old', 'incremental']].sum().reset_index()
+        chart_data = chart_data.sort_values(by = x_axis, ascending = False)
+        st.line_chart(chart_data, x = x_axis, y = [target_var_old, 'predicted_old', 'base_predict', 'incremental'])
+        
+    return data, key_counter
+
+def actual_vs_predicted_block(data, granularity_list, key_counter, column_list, target_var_old):
+    
+    temp = data.copy()
+    
+    with st.expander('Actual Vs Predicted: '):
+        st.write('')
+        st.write('Filter for AVP Plot')
+        
+        # st.write(column_list)
+        filter_cols = st.multiselect('Select Columns for filter', column_list, key = key_counter, )
+        key_counter = key_counter + 1
+        
+
+        for filter_col in filter_cols:
+            type_of_col = str(data[filter_col].dtype)
+            
+            if ('int' in type_of_col) or ('float' in type_of_col):
+                
+                start_val, end_val = st.select_slider('Select Range for '+filter_col, \
+                                                      range(0, int(data[filter_col].max()+1)), \
+                                                      value = (0, int(data[filter_col].max()-1)), \
+                                                      key = key_counter)
+                key_counter = key_counter + 1
+                    
+                temp = temp[(temp[filter_col] >= start_val) & (temp[filter_col] <= end_val)]
+            
+            else:
+                
+                filter_values = st.multiselect('Select Values for '+ filter_col, data[filter_col].unique(), \
+                                               key = key_counter)
+                key_counter = key_counter + 1
+                temp = temp[temp[filter_col].isin(filter_values)]
+        
+
+        x_axis = st.selectbox('Select Column for X Axis ', column_list, key = key_counter)
+        key_counter = key_counter + 1
+        
+        
+        chart_data = temp.groupby(x_axis)[[target_var_old, 'predicted_old', 'abs_error']].sum().reset_index()
+        chart_data = chart_data.sort_values(by = x_axis, ascending = False)
+        st.line_chart(chart_data, x =x_axis, y = [target_var_old, 'predicted_old', 'abs_error'])
+        
+        
+        return key_counter
+    
+    
+def variable_contribution_block(data, model, feature_list, exclude_from_model, feat_mono_dict, 
+                                granularity_list, target_var, target_var_old, key_counter):
+    
+    
+    with st.expander('Feature wise Contribution: '):
+        st.write('')
+        
+        simulation_variables = []
+        
+        # st.write(len(feature_list), len(granularity_list), len(exclude_from_model))
+        
+        for var in feature_list:
+            if var not in granularity_list:
+                if exclude_from_model[var]:
+                    pass
+                else:
+                   simulation_variables.append(var) 
+                            
+        if target_var in simulation_variables:
+            simulation_variables.remove(target_var)
+        
+        # st.write(len(simulation_variables))
+        features_selected = st.multiselect(
+            'Select Variable: ', list(set(simulation_variables)), key = key_counter)
+        key_counter = key_counter + 1
+        
+        for feature_ in features_selected:
+            data = get_base_contribution(data, model, feature_list, feat_mono_dict, feature = feature_)
+        
+        inc_col_list = [x for x in data.columns if 'inc_' in x]
+        # inc_col_list = inc_col_list + [target_var_old]
+        inc_df= data[inc_col_list].sum().reset_index()
+        
+        inc_df.columns = ['Feature', 'Contribution']
+        inc_df['% Contribution'] = np.round((inc_df['Contribution']/data[target_var_old].sum())*100, 2)
+        st.write(inc_df)
+        # sim_chart_data = simulation_data.sum()
+        # st.line_chart(sim_chart_data)
+        
+        return data, key_counter
+    
+    
+def download_model_data_block(data):
+    
+    with st.expander('Download output file : '):
+        
+        csv = convert_df(data)
+        
+        st.download_button(
+           "Press to Download",
+           csv,
+           "file.csv",
+           "text/csv",
+           key='download-csv'
+        )
+        
+    return None
